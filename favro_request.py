84eca41224bf9b84eb36ca8bfc12781dae7601b0
tinks <tine@Tines-MacBook-Pro.local>
2023-05-26 13:48:20 +0200

    Handle reaching the request limit per hour

--- favro_request.py
+++ favro_request.py
@@ -2,6 +2,8 @@ import requests
 import json
 from config import API_KEY, ORGANIZATION_ID
 from sys import exit
+import datetime
+import time
 
 def make_request(url, query_params = {}):
     my_headers = {'Authorization': API_KEY, 'organizationId': ORGANIZATION_ID}
@@ -22,20 +24,41 @@ def make_request(url, query_params = {}):
         print(response.request)
         print(response.headers)
         exit()
-    elif (response.status_code != 403):
-        print(response.status_code)
-        print(response.headers)
-        exit()
+    elif (response.status_code == 429): # handle reaching the request limit per hour
+        reset_time = datetime.datetime.fromisoformat(response.headers['X-RateLimit-Reset'])
+        now = datetime.datetime.now(datetime.timezone.utc)
+        diff = reset_time - now
+        time.sleep(diff)
+        make_request(url, query_params)
     
     # requests are limited to 100 entities, if there are more, then pages is greater than 1
     # to request the next page, you also need to the requestId from the first response
-    page = 1
+    if pages > 1:
+        request_all_pages(url, my_headers, query, entities , pages)
+
+    return entities
+
+def request_all_pages(url, my_headers, query_params, entities, pages, page = 1):
+
+    query = query_params
+
     while page < pages:
         query['page'] = page
         response = requests.get(url, params=query, headers=my_headers)
         if (response.status_code == 200):
             raw = json.loads(response.text)
             entities.extend(raw['entities'])
+        elif (response.status_code == 400 or response.status_code == 500 ):
+            print(response.status_code)
+            print(response.request)
+            print(response.headers)
+            exit()
+        elif (response.status_code == 429): # handle reaching the request limit per hour
+            reset_time = datetime.datetime.fromisoformat(response.headers['X-RateLimit-Reset'])
+            now = datetime.datetime.now(datetime.timezone.utc)
+            diff = reset_time - now
+            time.sleep(diff)
+            request_all_pages(url, my_headers, query_params, entities, pages, page)
         page += 1
     
     return entities
